1668e40892332f84bf75d28016b9c0954bffe037
Andrea <andreapasquini6@gmail.com>
2026-02-21 11:12:31 +0100

    Created handlers throwing bad request

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -2,14 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class MyController extends Controller
 {
-    public function myHandler(Request $request, ?User $user = null): int
+    /**
+     * Summary of myHandler
+     * @param Request $request
+     * @param mixed $user
+     * @return int
+     * @throws NotFoundHttpException
+     * @throws BadRequestHttpException
+     */
+    public function myHandler(Request $request): int
     {
         $bool = $request->boolean('test_param');
-        return $bool ? $user->id : -1;
+        $this->doSomething($bool);
+
+        return 1;
+    }
+
+    /**
+     * Summary of myOtherHandler
+     * @param Request $request
+     * @return int
+     * @throws NotFoundHttpException
+     * @throws BadRequestHttpException
+     */
+    public function myOtherHandler(Request $request): int
+    {
+        $bool = $request->boolean('test_param');
+        $this->doSomething($bool);
+
+        return 2;
+    }
+
+    private function doSomething(bool $param): void
+    {
+        if ($param) {
+            throw new BadRequestHttpException();
+        }
+
+        throw new NotFoundHttpException();
     }
 }
